87ee843166513ec002e0ce3eb049c2682647fdc5
Adryan Portella <adryanportella8@gmail.com>
2025-04-29 21:04:49 -0300

    Feat: Estrutura de conexão com o banco criada, variáveis de ambiente configuradas e conexão validada via DBeaver

---
+++ src/database/index.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from "sequelize";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASS as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql", // ou "postgres", "sqlite", etc.
+    logging: false,
+  }
+);
+
+export const connectDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+  } catch (error) {
+    console.error("Erro ao conectar com o banco de dados:", error);
+  }
+};
